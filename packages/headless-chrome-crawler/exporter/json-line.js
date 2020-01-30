cd7b12c59d1e3abcd3bb5a6eaef3d09d822e64fd
Beliayeu, Andrei [JJCUS Non-J&J] <abeliaye@ITS.JNJ.com>
2020-01-30 21:38:19 +0300

    fix linting issues

--- packages/headless-chrome-crawler/exporter/json-line.js
+++ packages/headless-chrome-crawler/exporter/json-line.js
@@ -10,10 +10,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * Copyright (c) 2017 yujiosaka
  * Licensed under MIT.
- * 
+ *
  */
 
 const pick = require('lodash/pick');
