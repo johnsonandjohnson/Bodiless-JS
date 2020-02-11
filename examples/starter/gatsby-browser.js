8d8fa7558a5bf9b1b06c0c7bb21eabfe7dfd3a0f
Dewen Li <dli94@its.jnj.com>
2020-02-11 15:17:56 -0500

    Migration tool: issues in FAQ pages

--- examples/starter/gatsby-browser.js
+++ examples/starter/gatsby-browser.js
@@ -1,4 +1,19 @@
 /**
+ * Copyright © 2020 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable import/prefer-default-export */
+/**
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
@@ -34,9 +49,9 @@ export const shouldUpdateScroll = ({ prevRouterProps, routerProps: { location } 
       try {
         // eslint-disable-next-line
         const { selectors } = require('./no-scroll-settings.json');
-        selectors.map(item => {
+        selectors.forEach(item => {
           document.querySelectorAll(item).forEach(element => {
-            if (element.attributes.href.value === '#' + hash) {
+            if (element.attributes.href.value === `#${hash}`) {
               throw HashMatchException;
             }
           });
