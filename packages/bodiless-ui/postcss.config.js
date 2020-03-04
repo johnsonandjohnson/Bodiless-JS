fcc7e04107415601f00ec323f3b47b8526b0bc82
Beliayeu, Andrei [JJCUS Non-J&J] <abeliaye@ITS.JNJ.com>
2020-03-04 13:54:00 +0300

    removed babel-plugin-tailwind-components from test-site

--- packages/bodiless-ui/postcss.config.js
+++ packages/bodiless-ui/postcss.config.js
@@ -12,11 +12,12 @@
  * limitations under the License.
  */
 
+// eslint-disable-next-line
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.jsx',
-    './src/**/*.tsx'
+    './src/**/*.tsx',
   ],
 
   // Include any special characters you're using in this regular expression
@@ -25,7 +26,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
+    // eslint-disable-next-line
     require('tailwindcss')('bodiless.tailwind.config.js'),
-    purgecss
+    purgecss,
   ],
 };
