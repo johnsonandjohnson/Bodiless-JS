9a2fde8170dfa49fe41a2581e6e63dfe0c49d2db
Ivan Rudiuk <IRudiuk@its.jnj.com>
2020-03-09 21:31:12 -0400

    Add test for accordion icon toggle

--- packages/bodiless-organisms/__tests__/SingleAccordion.test.tsx
+++ packages/bodiless-organisms/__tests__/SingleAccordion.test.tsx
@@ -39,4 +39,11 @@ describe('Single Accordion organizm', () => {
     expect(wrapper.render().hasClass('collapsed')).toBeTruthy();
     expect(wrapper.find('div[data-accordion-element="accordion-body-wrapper"]').hasClass('hidden')).toBeTruthy();
   });
+  it("should toggle accordion icons on toggle", () => {
+    const wrapper = mount(<SingleAccordion nodeKey="test" />);
+    wrapper.find('div[data-accordion-element="accordion-title-wrapper"]').simulate('click');
+    expect(wrapper.find('span[data-accordion-element="accordion-icon"]').text().includes('remove')).toBe(true);
+    wrapper.find('div[data-accordion-element="accordion-title-wrapper"]').simulate('click');
+    expect(wrapper.find('span[data-accordion-element="accordion-icon"]').text().includes('add')).toBe(true);
+  });
 });
