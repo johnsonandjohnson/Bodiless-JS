71cd61ed24d233920100b2a1e9e7ce6595d87dba
Ali <aalmusta@its.jnj.com>
2020-03-25 15:11:20 -0400

    fix datalayer name

--- examples/test-site/gatsby-config.js
+++ examples/test-site/gatsby-config.js
@@ -40,12 +40,10 @@ if (tagManagerEnabled) {
       // datalayer to be set before GTM is loaded
       // should be an object or a function that is executed in the browser
       // Defaults to null
-      defaultDataLayer: {},
-
       // Specify optional GTM environment details.
       // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
       // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-      dataLayerName: 'digitalData',
+      dataLayerName: 'globalDataLayer',
     },
   });
 }
