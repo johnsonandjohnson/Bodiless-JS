1770aee3c0edb3352b654cdc32fb7709b86e74be
Ivan Rudiuk <IRudiuk@its.jnj.com>
2020-04-06 14:01:15 -0400

    Add expires for /__docs, remove console.log

--- packages/bodiless-psh/src/bodiless-psh-init.ts
+++ packages/bodiless-psh/src/bodiless-psh-init.ts
@@ -56,7 +56,6 @@ const mergeByKey = (Source: any, Destination: any, Whitelist: any) => {
             console.log(`Key '${key}' is not whitelisted and not found in '@bodiless/psh' defaults. The value of ${Destination[key]} will be used.`);
             Object.assign(result, { [key]: Destination[key] });
           } else {
-            console.log(`Key '${key}' is not whitelisted. The default value of '${Source[key]}' from '@bodiless/psh' will be used.`);
             Object.assign(result, { [key]: Source[key] });
           }
         }
