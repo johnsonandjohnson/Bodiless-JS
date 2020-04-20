fad138f5b861b4328d24d5498d76e6bd76e3f3ff
Ivan Rudiuk <IRudiuk@its.jnj.com>
2020-04-20 14:04:57 -0400

    Implement bl-prevent attribute to control preventDefault in useContextActivator()

--- packages/bodiless-core/src/hooks.ts
+++ packages/bodiless-core/src/hooks.ts
@@ -20,17 +20,12 @@ export const useEditContext = () => useContext(PageEditContext.context);
 
 export const useUUID = () => useRef(v1()).current;
 
-export type EventConfig = {
-  preventDefault: boolean,
-};
-
 export const useContextActivator = (
   event = 'onClick',
   handler?: EventHandler<any>,
-  eventConfig?: EventConfig,
 ) => {
   const context = useEditContext();
-  const preventDefault = Boolean(eventConfig && eventConfig.preventDefault !== false);
+
   // Don't attach the handler when not in edit mode.
   // @TODO: Find a better way to keep the outermost context active even when not editing. AESQ-537
   if (!context.isEdit && context.name !== 'page') {
@@ -39,6 +34,8 @@ export const useContextActivator = (
     };
   }
   const handler$1 = (e: React.SyntheticEvent<any>) => {
+    const preventDefault = e.currentTarget.getAttribute('bl-prevent') !== 'false';
+
     if (handler) handler(e);
     context.activate();
     if (e && e.stopPropagation) e.stopPropagation();
