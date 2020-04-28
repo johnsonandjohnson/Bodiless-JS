007dbb18525c3605e268ba0a5f26b3d1fadee760
Ali <aalmusta@its.jnj.com>
2020-04-28 12:14:28 -0400

    address feedback

--- packages/bodiless-components/__tests__/withFilterByTags.test.tsx
+++ packages/bodiless-components/__tests__/withFilterByTags.test.tsx
@@ -1,6 +1,8 @@
 import React, { useState, FC } from 'react';
 import { flow } from 'lodash';
-import { withNode, DefaultContentNode, NodeProvider,TagType } from '@bodiless/core';
+import {
+  withNode, DefaultContentNode, NodeProvider, TagType,
+} from '@bodiless/core';
 
 import { mount } from 'enzyme';
 import withFilterByTags from '../src/withFilterByTags';
@@ -40,16 +42,16 @@ const TestFilterSelector = () => {
     <div>
       <div>
         <h2>Select a tag to filter by</h2>
-        <button id="show-foo" type="button" onClick={() => setTags([{ id:'fooID', name:'foo'}])}>
+        <button id="show-foo" type="button" onClick={() => setTags([{ id: 'fooID', name: 'foo' }])}>
           foo
         </button>
-        <button id="show-bar" type="button" onClick={() => setTags([{ id:'barID', name:'bar'}])}>
+        <button id="show-bar" type="button" onClick={() => setTags([{ id: 'barID', name: 'bar' }])}>
           bar
         </button>
-        <button id="show-baz" type="button" onClick={() => setTags([{ id:'barID', name:'bat'}])}>
+        <button id="show-baz" type="button" onClick={() => setTags([{ id: 'barID', name: 'bat' }])}>
           baz
         </button>
-        <button id="show-bat" type="button" onClick={() => setTags([{ id:'bazID', name:'baz'}])}>
+        <button id="show-bat" type="button" onClick={() => setTags([{ id: 'bazID', name: 'baz' }])}>
           bat
         </button>
       </div>
