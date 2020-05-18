601f89203696544d370fc8d7851d42911678b9cd
Dewen Li <dli94@its.jnj.com>
2020-05-18 08:09:51 -0400

    update docs

--- packages/bodiless-backend/__tests__/getConflict.test.ts
+++ packages/bodiless-backend/__tests__/getConflict.test.ts
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { cloneGitFixture, cleanGitFixture } from './tools';
 
 const { getConflicts } = require('../src/git');
