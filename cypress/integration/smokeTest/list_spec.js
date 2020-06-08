7cc08b779f40a3de6d03be451c6191ab6938a171
Ivan Rudiuk <IRudiuk@its.jnj.com>
2020-06-08 10:37:42 -0400

    Fix cypress tests

--- cypress/integration/smokeTest/list_spec.js
+++ cypress/integration/smokeTest/list_spec.js
@@ -30,7 +30,7 @@ describe('List (text and link versions) testing', function () {
   const listItemUrl = 'AT-listItemUrl'
   const itemCount = 2
   const addListItemIcon = '//*[@role="toolbar" and @aria-label="Local Context Menu"]//*[@aria-label="Add"]'
-  const addSubListItemIcon = '//*[@role="toolbar" and @aria-label="Local Context Menu"]//*[@aria-label="Sublist"]'
+  const addSubListItemIcon = '//*[@role="toolbar" and @aria-label="Local Context Menu"]//*[@aria-label="Sub"]'
   const removeListItemIcon = '//*[@role="toolbar" and @aria-label="Local Context Menu"]//*[@aria-label="Delete"]'
   const linkIcon = '//*[@role="toolbar" and @aria-label="Local Context Menu"]//*[@aria-label="Link"]'
   const urlField = '//form[@aria-label="Context Menu Link Form"]//input[@id="link-href"]'
