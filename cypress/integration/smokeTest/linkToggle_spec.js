6564c7d630addea1fab80c9daf25094221f330e3
Ivan Rudiuk <IRudiuk@its.jnj.com>
2020-08-24 12:15:17 -0400

    Remove unnecessary test step

--- cypress/integration/smokeTest/linkToggle_spec.js
+++ cypress/integration/smokeTest/linkToggle_spec.js
@@ -143,7 +143,6 @@ describe('Link Toggle smoke tests', function () {
       .should('not.exist');
     cy.xpath(labelPreviewXpath)
       .should('have.text', label + editedPostfix);
-    cy.visit('/link-toggle/'); // Comment to toggle Build
     cy.xpath(labelPreviewXpath)
       .click();
     cy.url().should('eq', Cypress.config().baseUrl + '/link-toggle/');
