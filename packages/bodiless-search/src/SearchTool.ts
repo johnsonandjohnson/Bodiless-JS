81471e1515e28ffc0912444fd4850335a1d9a5ad
Dewen Li <dli94@its.jnj.com>
2020-11-02 12:32:34 -0500

    remove special char from indexing

--- packages/bodiless-search/src/SearchTool.ts
+++ packages/bodiless-search/src/SearchTool.ts
@@ -125,6 +125,7 @@ class SearchTool {
       return (this.type === 'text') ? $(this).text().trim() : '';
     }).get()
       .join(' ')
+      .replace(/:|^|\*|\+|-|~|%|"/gi, ' ')
       .replace(/ +/gi, ' ')
       .trim();
 
