2e243c0700b64cf5da89f8168c6eed6e501ae0ed
Ali <aalmusta@its.jnj.com>
2021-03-30 16:09:36 -0400

    Initial work on anchor hoc

---
+++ examples/test-site/src/data/pages/anchor/index.tsx
@@ -0,0 +1,43 @@
+/**
+ * Copyright © 2019 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import { flow } from 'lodash';
+import { graphql } from 'gatsby';
+import { Page } from '@bodiless/gatsby-theme-bodiless';
+import { asBodilessAnchor } from '@bodiless/components';
+import { H1, Span } from '@bodiless/fclasses';
+import Layout from '../../../components/Layout';
+import { asHeader1 } from '../../../components/Elements.token';
+
+const ExampleAnchor = flow(
+  asBodilessAnchor('foo'),
+)(Span);
+const Title = asHeader1(H1);
+
+export default (props: any) => (
+  <Page {...props}>
+    <Layout>
+      <Title>Link Demo Page</Title>
+      <ExampleAnchor>TEST ANCHOR</ExampleAnchor>
+    </Layout>
+  </Page>
+);
+
+export const query = graphql`
+  query($slug: String!) {
+    ...PageQuery
+    ...SiteQuery
+    ...DefaultContentQuery
+  }
+`;
---
+++ packages/bodiless-components/src/Anchor/asBodilessAnchor.tsx
@@ -0,0 +1,54 @@
+// @ts-nocheck
+/**
+ * Copyright © 2020 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { HTMLProps } from 'react';
+import {
+  withContextActivator,
+  withNode,
+  withNodeDataHandlers,
+  withLocalContextMenu,
+  WithNodeProps,
+  ifEditable,
+  Bodiless,
+  withNodeKey,
+  withoutProps,
+} from '@bodiless/core';
+import { flowRight } from 'lodash';
+import withAnchorButton from './withAnchorButton';
+// Type of the data used by this component.
+// @Todo: Determine if this type is necessary?
+type Props = HTMLProps<HTMLElement>;
+
+// Composed hoc which creates editable version of the component.
+// Note - the order is important. In particular:
+// - the node data handlers must be outermost
+// - anything relying on the context (activator, indicator) must be
+//   *after* `withEditButton()` as this establishes the context.
+// - withData must be *after* the data handlers are defiend.
+const asBodilessAnchor = (nodeKey?: string) => flowRight(
+  withNodeKey(nodeKey),
+  withNode,
+  withNodeDataHandlers(''),
+  ifEditable(
+    withAnchorButton,
+    withContextActivator('onClick'),
+    withLocalContextMenu,
+  ),
+  withoutProps([
+    'componentData',
+    'setComponentData',
+  ]),
+) as Bodiless<Props, Props & Partial<WithNodeProps>>;
+export default asBodilessAnchor;
