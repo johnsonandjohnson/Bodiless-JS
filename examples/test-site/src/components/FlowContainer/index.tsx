b11a90f05e883e8070451d8f4afa0ef4b3e5da23
Dewen Li <dli94@its.jnj.com>
2021-10-29 09:25:00 -0400

    Nested Flow Container with library

--- examples/test-site/src/components/FlowContainer/index.tsx
+++ examples/test-site/src/components/FlowContainer/index.tsx
@@ -54,6 +54,7 @@ const FlowContainerDefaultWithContentLibraryRTL = asToken(
     }),
   ),
   asDefaultFlowContainer,
+  withLibraryFlowContainerVariations,
   asFlowContainerRTL,
 )(FlowContainer);
 
