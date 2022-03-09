905fce315f29dd0c5984ae10b1a4bc3281b7c00b
Konstantin Mishur <kmishur@its.jnj.com>
2022-03-09 15:15:18 +0300

    Revert temp changes

--- packages/bodiless-card/bodiless.env.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  configure: async (defaultConfig) => ({
-    ...defaultConfig,
-    BODILESS_CARD_TEST_ENV_CONFIG: '0',
-  })
-};
--- packages/bodiless-card/src/getBodilessEnvConfig.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { getPackageEnvConfig } from '@bodiless/cli/lib/helpers/getPackageEnvConfig';
-
-const packageRootDir = path.resolve(__dirname, '..');
-
-export const getBodilessEnvConfig = () => getPackageEnvConfig(
-  packageRootDir,
-);
