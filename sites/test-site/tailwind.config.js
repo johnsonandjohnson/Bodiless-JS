4569e8f3336e9acbce9e218d9056147881ef9a56
Konstantin Mishur <kmishur@its.jnj.com>
2022-03-15 16:51:58 +0300

    Comment out example code

--- sites/test-site/tailwind.config.js
+++ sites/test-site/tailwind.config.js
@@ -6,8 +6,8 @@ const { getPackageTailwindConfig, mergeConfigs } = requireEsm(
 
 // Requre configs files directly in order to watch for changes and to update the page
 // instantly (in "live mode") during development:
-const siteConfig = require('./site.tailwind.config');
-const accordionConfig = require('../../packages/bodiless-accordion/site.tailwind.config');
+// const siteConfig = require('./site.tailwind.config');
+// const accordionConfig = require('../../packages/bodiless-accordion/site.tailwind.config');
 // const someOtherPackageConfigYouWantToWatchOn = require(<relative_path_to_package>);
 
 // Get configs sorted by precedence and/or exclude some packages:
