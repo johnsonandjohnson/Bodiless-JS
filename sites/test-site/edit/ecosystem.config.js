9756e637c78effbdc0fba91f71a34a18567abbcc
Konstantin Mishur <kmishur@its.jnj.com>
2022-03-28 03:25:41 +0300

    Add env variable to switch which site deploys in bodiless

--- sites/test-site/edit/ecosystem.config.js
+++ sites/test-site/edit/ecosystem.config.js
@@ -1,18 +1,27 @@
+const fs = require('fs');
+
 const appVolume = () => process.env.APP_VOLUME || '/app/volume';
 const gatsbyPort = () => process.env.PORT || 8888;
+const siteName = () => process.env.APP_SITE_NAME || 'test-site';
+const backend = () => {
+  const backendRelative = 'node_modules/@bodiless/backend/src/server.js';
+  const siteLevelBackend = `${appVolume()}/root/sites/${siteName()}/${backendRelative}`;
+  return fs.existsSync(siteLevelBackend)
+    ? siteLevelBackend : `${appVolume()}/root/${backendRelative}`;
+};
 
 module.exports = {
   apps: [
     {
       name: 'frontend',
-      cwd: `${appVolume()}/root/sites/test-site`,
+      cwd: `${appVolume()}/root/sites/${siteName()}`,
       script: `${appVolume()}/root/node_modules/.bin/gatsby`,
       args: `develop --port ${gatsbyPort()}`,
     },
     {
       name: 'backend',
-      cwd: `${appVolume()}/root/sites/test-site`,
-      script: `${appVolume()}/root/packages/bodiless-backend/src/server.js`,
+      cwd: `${appVolume()}/root/sites/${siteName()}`,
+      script: backend(),
     },
   ],
 };
