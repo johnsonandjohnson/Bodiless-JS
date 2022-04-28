f6d0db21cabd77f2aeeb1bdbd688e355a5f86d1f
hvanyo <hvanyo@its.jnj.com>
2022-04-28 16:43:30 -0600

    Clean up ProductItemView

--- packages/bodiless-ga4/src/GA4-events/ProductItemView.tsx
+++ packages/bodiless-ga4/src/GA4-events/ProductItemView.tsx
@@ -2,6 +2,7 @@
  * @file Example definition of a Product GA4 Datalayer Component with editable
  * fields.
  */
+import { HOC } from '@bodiless/fclasses';
 import Helmet from 'react-helmet';
 import withDataLayerItem, { withDefaultDataLayer } from '../gtm';
 import { withGlobalGA4Form } from '../util';
@@ -17,47 +18,26 @@ const productDefaultDataLayer = {
   },
 };
 
-// Define a product Item/ID editable field to be added to the GA4 form.
-const withDataLayerItemID = withDataLayerItem({
-  name: 'id',
-  label: 'Product/Item ID',
-  path: 'productObject.ecommerce.items.0.item_id',
-});
+const productItems = [
+  { name: 'id', label: 'Product/Item ID', path: 'productObject.ecommerce.items.0.item_id' },
+  { name: 'upc', label: 'Product/Item SKU', path: 'productObject.ecommerce.items.0.upc' },
+  { name: 'GTIN', label: 'Product/Item GTIN', path: 'productObject.ecommerce.items.0.gtin' },
+  { name: 'item_name', label: 'Product/Item Name', path: 'productObject.ecommerce.items.0.item_name' },
+  { name: 'category', label: 'Product/Item Category', path: 'productObject.ecommerce.items.0.item_category' },
+  { name: 'variant', label: 'Product/Item Variant Name', path: 'productObject.ecommerce.items.0.variant' },
+  { name: 'affiliation', label: 'Product/Item Affiliation', path: 'productObject.ecommerce.items.0.affiliation' },
+  { name: 'currency', label: 'Product/Item Currency', path: 'productObject.ecommerce.items.0.currency' },
+  { name: 'item_out_of_stock', label: 'Product/Item Out of Stock', path: 'productObject.ecommerce.items.0.item_out_of_stock' },
+  { name: 'item_subscription_type', label: 'Product/Item Subscription Type', path: 'productObject.ecommerce.items.0.item_subscription_type' },
+  { name: 'location_id', label: 'Product/Item Location ID', path: 'productObject.ecommerce.items.0.location_id' },
+  { name: 'price', label: 'Product/Item Price', path: 'productObject.ecommerce.items.0.price' },
+  { name: 'quantity', label: 'Product/Item Quantity', path: 'productObject.ecommerce.items.0.quantity' },
+  { name: 'brand', label: 'Product/Item Brand', path: 'productObject.ecommerce.items.0.brand' },
+];
 
-// Define a product UPC editable field to be added to the GA4 form.
-const withDataLayerSKU = withDataLayerItem({
-  name: 'upc',
-  label: 'Product/Item SKU',
-  path: 'productObject.ecommerce.items.0.sku',
-});
+const items = Object.entries(productItems);
 
-// Define a product GTIN editable field to be added to the GA4 form.
-const withDataLayerGTIN = withDataLayerItem({
-  name: 'sku',
-  label: 'Product/Item GTIN',
-  path: 'productObject.ecommerce.items.0.gtin',
-});
-
-// Define a product Name editable field to be added to the GA4 form.
-const withDataLayerProductName = withDataLayerItem({
-  name: 'item_name',
-  label: 'Product/Item Name',
-  path: 'productObject.ecommerce.items.0.item_name',
-});
-
-// Define a product category editable field to be added to the GA4 form.
-const withDataLayerCategory = withDataLayerItem({
-  name: 'category',
-  label: 'Product/Item Category',
-  path: 'productObject.ecommerce.items.0.item_category',
-});
-
-// Define a product variant editable field to be added to the GA4 form.
-const withDataLayerProductVariant = withDataLayerItem({
-  name: 'variant',
-  label: 'Product/Item Variant Name',
-  path: 'productObject.ecommerce.items.0.variant',
-});
+const dataLayerItems = items.map(([key, item]) => withDataLayerItem(item)(item.name) as HOC);
 
 /**
  * A helmet Component containing datalayer script. In edit mode, it shows a form
@@ -66,51 +46,7 @@ const withDataLayerProductVariant = withDataLayerItem({
  * The use of withGlobalGA4Form allows us to retain the global datalayer script
  * and only add product information to it.
  */
-
 export const GA4DataLayerProductItemHelmet = withGlobalGA4Form(
   withDefaultDataLayer(productDefaultDataLayer),
-  withDataLayerItemID('product-id'),
-  withDataLayerSKU('product-sku'),
-  withDataLayerGTIN('product-gtin'),
-  withDataLayerProductName('product-name'),
-  withDataLayerCategory('product-category'),
-  withDataLayerProductVariant('product-variant'),
-)(Helmet);
-
-/* TODO
-ADD:
-  affiliation
-  currency
-  item_brand
-  item_out_of_stock
-  item_subscription_type
-  location_id
-  price
-  quantity
-
-const productItems = [
-  { name: 'id', label: 'Product/Item ID', path: 'productObject.ecommerce.items.0.item_id' },
-  { name: 'upc', label: 'Product/Item SKU', path: 'productObject.ecommerce.items.0.upc' },
-];
-
-const withMultipleProductItems = productItems.map(item => withDataLayerItem({ ...item }));
-
-or
-
-const withMultipleProductItems = productItems.forEach((item) => {
-  withDataLayerItem({ ...item });
-});
-
-or
-
-// @ts-ignore
-const dataLayerItems = [];
-
-productItems.map((item) => dataLayerItems.push(withDataLayerItem(item)));
-
-export const GA4DataLayerProductItemHelmet = withGlobalGA4Form(
-  withDefaultDataLayer(productDefaultDataLayer),
-  // @ts-ignore
   ...dataLayerItems,
 )(Helmet);
-*/
