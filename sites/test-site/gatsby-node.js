5407f1d6c9974ef3fae69afbdb05d168fdfe6171
Dewen Li <dli94@its.jnj.com>
2022-05-06 11:13:03 -0400

    fix(bodiles-psh): build issue

--- sites/test-site/gatsby-node.js
+++ sites/test-site/gatsby-node.js
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  *
  */
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+// const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
@@ -20,17 +20,17 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
     // so we ensure we always resolve to the same instance when present
     // in node_modules.  When running from sites, react is hoisted and
     // won't be present in the test site's node_modules.
-    const reactPath = path.resolve('./node_modules', 'react');
-    const reactAlias = fs.existsSync(reactPath) ? { react: reactPath } : {};
+    // const reactPath = path.resolve('./node_modules', 'react');
+    // const reactAlias = fs.existsSync(reactPath) ? { react: reactPath } : {};
     actions.setWebpackConfig({
       // Set devtool to `false` below to disable sourcemap on performance improvement.
       // or set devtool as 'cheap-module-source-map' to re-enable sourcemap.
       // See https://webpack.js.org/configuration/devtool/
       devtool: false,
-      resolve: {
-        plugins: [new TsconfigPathsPlugin()],
-        alias: reactAlias,
-      },
+      // resolve: {
+      //   plugins: [new TsconfigPathsPlugin()],
+      //   alias: reactAlias,
+      // },
       // On development, we want changes on Bodiless packages to trigger
       // new builds. Webpack won't watch packages inside node_modules by
       // default, so we remove the @bodiless folder from its default list.
