8976830c48a344338cb7857ceb217231cb0da132
Konstantin Mishur <kmishur@its.jnj.com>
2022-05-06 15:37:55 +0300

    use footer token instead of fluid, extend instead of override

--- packages/__vital__/src/shadow/@bodiless/vital-layout/Footer.ts
+++ packages/__vital__/src/shadow/@bodiless/vital-layout/Footer.ts
@@ -11,28 +11,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { asFluidToken } from '@bodiless/vital-elements';
-import { vitalFooterBase } from '@bodiless/vital-layout';
+import { vitalFooterBase, asFooterToken } from '@bodiless/vital-layout';
 import {
   addProps, as
 } from '@bodiless/fclasses';
 import { withLanguageNode } from '@bodiless/i18n';
 
-const Default = asFluidToken({
-  ...vitalFooterBase.Default,
+const Default = asFooterToken(vitalFooterBase.Default, {
   Core: {
-    ...vitalFooterBase.Default.Core,
     _: addProps({ 'data-shadowed-by': '__vital__Footer' }),
   },
   Schema: {
-    ...vitalFooterBase.Default.Schema,
     FooterMenu: as(
       withLanguageNode,
-      vitalFooterBase.Default.Schema.FooterMenu
     ),
     Copyright: as(
       withLanguageNode,
-      vitalFooterBase.Default.Schema.Copyright,
     ),
   },
 });
