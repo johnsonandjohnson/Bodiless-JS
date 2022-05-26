fb6d06b7246902f9c3f1ca9dfdf09da063071858
hvanyo <hvanyo@its.jnj.com>
2022-05-26 08:14:42 -0500

    add shadowing for H1 - H5

--- packages/vital-test/src/shadow/@bodiless/vital-elements/Typography.ts
+++ packages/vital-test/src/shadow/@bodiless/vital-elements/Typography.ts
@@ -19,7 +19,27 @@ export default {
   ...vitalTypographyBase,
   H1: asElementToken(vitalTypographyBase.H1, {
     Theme: {
-      _: addProps({ 'data-shadowed-by': '__vital__:Typography' }),
+      _: addProps({ 'data-shadowed-by': '__vital__:Typography:H1' }),
+    },
+  }),
+  H2: asElementToken(vitalTypographyBase.H2, {
+    Theme: {
+      _: addProps({ 'data-shadowed-by': '__vital__:Typography:H2' }),
+    },
+  }),
+  H3: asElementToken(vitalTypographyBase.H3, {
+    Theme: {
+      _: addProps({ 'data-shadowed-by': '__vital__:Typography:H3' }),
+    },
+  }),
+  H4: asElementToken(vitalTypographyBase.H4, {
+    Theme: {
+      _: addProps({ 'data-shadowed-by': '__vital__:Typography:H4' }),
+    },
+  }),
+  H5: asElementToken(vitalTypographyBase.H5, {
+    Theme: {
+      _: addProps({ 'data-shadowed-by': '__vital__:Typography:H5' }),
     },
   }),
 };
