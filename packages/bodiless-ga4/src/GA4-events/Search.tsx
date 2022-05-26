a741542b83f1be154d38dcac60635044951c1baa
hvanyo <hvanyo@its.jnj.com>
2022-05-26 11:45:48 -0500

    add search events

--- packages/bodiless-ga4/src/GA4-events/Search.tsx
+++ packages/bodiless-ga4/src/GA4-events/Search.tsx
@@ -15,28 +15,20 @@
 import { addProps } from '@bodiless/fclasses';
 import { pushSearchAnalytics } from '../util';
 
-export const withSearchDataLayer = {
-  Behavior: {
-    _: addProps({
-      onSubmit: (value: any) => {
-        pushSearchAnalytics({
-          corrected_term: '',
-          search_term: value,
-        });
-      },
-    }),
+export const addSearchDataLayer = addProps({
+  onSubmit: (value: any) => {
+    pushSearchAnalytics({
+      corrected_term: '',
+      search_term: value,
+    });
   },
-};
+});
 
-export const withSearchDataLayerSuggestion = {
-  Behavior: {
-    _: addProps({
-      additionalHandler: (v: any) => {
-        pushSearchAnalytics({
-          corrected_term: v.searchTerm,
-          search_term: v.text,
-        });
-      },
-    }),
+export const addSearchDataLayerSuggestion = addProps({
+  additionalHandler: (v: any) => {
+    pushSearchAnalytics({
+      corrected_term: v.searchTerm,
+      search_term: v.text,
+    });
   },
-};
+});
