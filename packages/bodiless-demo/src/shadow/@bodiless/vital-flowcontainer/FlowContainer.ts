f1cf28f1d62ddeff4fbe7e58617d9f57d7b73471
hvanyo <hvanyo@its.jnj.com>
2022-06-14 17:13:41 -0500

    add table content to bodiless-demo

--- packages/bodiless-demo/src/shadow/@bodiless/vital-flowcontainer/FlowContainer.ts
+++ packages/bodiless-demo/src/shadow/@bodiless/vital-flowcontainer/FlowContainer.ts
@@ -1,15 +1,13 @@
-import { useNode } from '@bodiless/core';
-import { flowIf, on } from '@bodiless/fclasses';
+import { on } from '@bodiless/fclasses';
 import { asFluidToken } from '@bodiless/vital-elements';
 import { vitalFlowContainerBase } from '@bodiless/vital-flowcontainer';
 import { CardClean } from '@bodiless/vital-card';
+import { TableClean } from '@bodiless/vital-table';
 import { StyledEditor, StyledEditorClean } from '../../../components/StyledEditor';
 import { asGetStarted } from '../../../components/Cards';
+import { asSecondColumnHighlighted, asThirdColumnHighlighted } from '../../../components/Tables';
 
-const isHome = () => useNode().node.pagePath === '/';
-
-const ExtraPaddingOnHome = asFluidToken(vitalFlowContainerBase.Default, {
-  Flow: flowIf(isHome),
+const ExtraPadding = asFluidToken(vitalFlowContainerBase.Default, {
   Spacing: {
     ComponentWrapper: 'my-12',
   },
@@ -22,9 +20,11 @@ const Default = asFluidToken(
       ...vitalFlowContainerBase.Default.Components,
       StyledEditorBorder: on(StyledEditorClean)(StyledEditor.Borders),
       GetStarted: on(CardClean)(asGetStarted),
+      SecondHighlightedTable: on(TableClean)(asSecondColumnHighlighted),
+      ThirdHighlightedTable: on(TableClean)(asThirdColumnHighlighted),
     },
   },
-  ExtraPaddingOnHome,
+  ExtraPadding,
 );
 
 export default {
