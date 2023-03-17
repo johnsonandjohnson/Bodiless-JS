c3ed8fd906851798e8620148e750e886cb139ebe
Uladzislau Arlouski <uarlouski@gmail.com>
2023-03-17 17:12:28 +0200

    fix: Address comments

--- playwright/pages/vital-typography.ts
+++ playwright/pages/vital-typography.ts
@@ -1,51 +1,41 @@
+/**
+ * Copyright © 2023 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { VitalElement, VitalPage } from './vital-page';
 
 export class VitalTypographyPage extends VitalPage {
-  readonly typographyElements: TypographyElement[];
+  readonly typographyElements: string[];
+
+  readonly linkId: string;
 
   constructor() {
     super('/styleguide/typography/');
+    this.linkId = 'Link';
     this.typographyElements = [
-      {
-        id: 'H1'
-      },
-      {
-        id: 'H2'
-      },
-      {
-        id: 'H3'
-      },
-      {
-        id: 'H4'
-      },
-      {
-        id: 'H5'
-      },
-      {
-        id: 'Body'
-      },
-      {
-        id: 'Eyebrow'
-      },
-      {
-        id: 'Gradient'
-      },
-      {
-        id: 'Rest'
-      },
-      {
-        id: 'Link',
-        isClickable: true
-      }
+      'H1',
+      'H2',
+      'H3',
+      'H4',
+      'H5',
+      'Body',
+      'Eyebrow',
+      'Gradient',
+      'Rest',
+      this.linkId
     ];
   }
 
   getElements(): VitalElement[] {
-    return this.typographyElements.map((tElement) => ({id: tElement.id}));
+    return this.typographyElements.map((tElement) => ({id: tElement}));
   }
 }
-
-type TypographyElement = {
-  id: string,
-  isClickable?: boolean
-};
--- playwright/tests/smoke/vital-typography.spec.ts
+++ playwright/tests/smoke/vital-typography.spec.ts
@@ -1,18 +1,27 @@
+/**
+ * Copyright © 2023 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { expect, test } from '@playwright/test';
 import { VitalTypographyPage } from '../../pages/vital-typography';
 
 test.describe('Vital Typography', () => {
-  const typographyPage: VitalTypographyPage = new VitalTypographyPage();
-
-  typographyPage.typographyElements.filter((tElement) => tElement.isClickable)
-    .forEach((tElement) => {
-      test(`Should click on ${tElement.id} typography element`, async ({ page }) => {
-        await page.goto(typographyPage.relativeUrl);
-        await page.waitForLoadState();
-        const element = page.getByTestId(tElement.id)
-          .locator('a');
-        await element.click({ noWaitAfter: false });
-        expect(page.url().endsWith('/test/')).toBeTruthy();
-      });
-    });
+  test('Should click on Link typography element', async ({ page }) => {
+    const typographyPage: VitalTypographyPage = new VitalTypographyPage();
+    await page.goto(typographyPage.relativeUrl);
+    await page.waitForLoadState();
+    const element = page.getByTestId(typographyPage.linkId)
+      .locator('a');
+    await element.click({ noWaitAfter: false });
+    expect(page.url().endsWith('/test/')).toBeTruthy();
+  });
 });
