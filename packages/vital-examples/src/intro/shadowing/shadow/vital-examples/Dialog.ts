2fa3fbc06706777f4c144891410dae498f4cfc9c
Chris Oden <coden@kenvue.com>
2023-05-11 13:24:43 -0400

    Add shadowing example.

---
+++ packages/vital-examples/src/intro/shadowing/shadow/vital-examples/Dialog.ts
@@ -0,0 +1,8 @@
+// import { exampleDialogBase } from '../../base';
+
+import { customDialog } from '../../components/CustomDialog';
+
+export default {
+  // ...exampleDialogBase,
+  ...customDialog,
+};
