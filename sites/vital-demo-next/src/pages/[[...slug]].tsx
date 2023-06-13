78f23937eade760713daa287568189034bc0d3bb
JJ Kasper <jj@jjsweb.site>
2023-06-12 19:06:56 -0700

    Update Next.js handling to match behavior

--- sites/vital-demo-next/src/pages/[[...slug]].tsx
+++ sites/vital-demo-next/src/pages/[[...slug]].tsx
@@ -1,25 +1,25 @@
 import getStaticProps from '@bodiless/next/lib/getStaticProps';
 import getStaticPaths from '@bodiless/next/lib/getStaticPaths';
+
 import PageRenderer from '@bodiless/next/lib/PageRenderer';
-import _default from '../templates/_default';
-import styleguide from '../templates/styleguide';
+import dynamic from 'next/dynamic';
+
+const Styleguide = dynamic(() => import('../templates/styleguide'));
+const Default = dynamic(() => import('../templates/_default'));
 
 const Templates = {
-  '_default.jsx': _default,
-  'styleguide.jsx': styleguide
-};
-
-export {
-  getStaticProps,
-  getStaticPaths
+  '_default.jsx': Default,
+  'styleguide.jsx': Styleguide,
 };
 
 const Page = ({ component, ...rest }: any) => {
-  const DefaultPage = Templates[component] || _default;
+  const DefaultPage = Templates[component] || Default;
   return PageRenderer({
     Component: DefaultPage,
-    ...rest
+    ...rest,
   });
 };
 
 export default Page;
+
+export { getStaticProps, getStaticPaths };
