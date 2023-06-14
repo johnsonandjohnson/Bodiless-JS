acbfa85f799b0263b421dd4eedc662cd5abe06ab
Luis Alvarez <luis@vercel.com>
2023-06-14 18:05:50 -0500

    Added getPageData and generateStaticParams

---
+++ packages/bodiless-next/src/app/generateStaticParams.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright © 2023 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  getPages,
+  getDisabledPages,
+  getRedirectAliases,
+} from '@bodiless/page/lib/cjs/NodeApi';
+
+import type { AliasItem } from '@bodiless/page';
+import { hasTrailingSlash } from '../nextConfig';
+
+export const generateStaticParams = async () => {
+  const pages = await getPages();
+  const disablePageList = getDisabledPages();
+  const disabledPages =
+    Object.keys(disablePageList).filter(
+      (item) => disablePageList[item].pageDisabled === true,
+    ) || [];
+  const activePages = pages.filter(
+    (page) =>
+      !(
+        process.env.NODE_ENV === 'production' &&
+        disabledPages.indexOf(hasTrailingSlash() ? `${page}/` : page) > -1
+      ),
+  );
+  const redirects = getRedirectAliases();
+
+  return [
+    ...activePages.map((page) => ({
+      slug: page.split('/').filter(Boolean) || [],
+    })),
+    ...redirects.map((redirect: AliasItem) => ({
+      slug: redirect.fromPath.split('/').filter(Boolean) || [],
+    })),
+  ];
+};
