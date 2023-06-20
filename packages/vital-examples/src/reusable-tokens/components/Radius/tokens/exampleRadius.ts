536fe791ef3221dbb59029acc6e6caa34a0fc954
mister. jones <misterjones@users.noreply.github.com>
2023-06-20 16:54:20 -0400

    Clean up "Reusable Tokens" lesson
    - Clean up "Reusable Tokens" curriculum lesson.
      - Fix typos, formatting, etc.
      - Embed associated example code files.
    - Update example code files.
      - Fix typos, formatting, etc. in the comments.
      - Restrict code comments to column 78 for Docsify.

--- packages/vital-examples/src/reusable-tokens/components/Radius/tokens/exampleRadius.ts
+++ packages/vital-examples/src/reusable-tokens/components/Radius/tokens/exampleRadius.ts
@@ -1,10 +1,10 @@
 import { asElementToken } from '@bodiless/vital-elements';
-/*
-* Below, we craft a simple element token that will apply
-* a 40px, bottom right border radius to any component on
-* which the token is applied.
-*/
 
+/// [radius-tokens]
+/*
+ * Below, we craft a simple element token that will apply a 40px, bottom-right
+ * border radius to any component on which the token is applied.
+ */
 const Simple = asElementToken({
   Theme: {
     _: 'rounded-bl-[40px]',
@@ -12,15 +12,15 @@ const Simple = asElementToken({
 });
 
 /*
-* Here, we craft a second reusable element token that will apply
-* the more complex CSS rules previously added to the tailwind
-* configuration file.
-*/
+ * Here, we craft a second reusable element token that will apply the more
+ * complex CSS rules previously added to the Tailwind configuration file.
+ */
 const Fancy = asElementToken({
   Theme: {
     _: 'card-corner md:card-corner-md lg:card-corner-lg',
   },
 });
+/// [radius-tokens]
 
 export default {
   Simple,
