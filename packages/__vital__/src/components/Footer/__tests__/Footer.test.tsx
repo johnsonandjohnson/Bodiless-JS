a5985af3ade3c3cc4e6350e931e99920f3436eb9
Dewen Li <dewen.sinocrest@gmail.com>
2023-07-06 13:13:23 -0400

    test update

--- packages/__vital__/src/components/Footer/__tests__/Footer.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { testTokens } from '@bodiless/vital-elements';
-import { FooterClean } from '@bodiless/vital-layout';
-import { __vital__Footer } from '..';
-
-// eslint-disable-next-line jest/valid-describe
-describe('Footer Tokens', testTokens(FooterClean, __vital__Footer));
