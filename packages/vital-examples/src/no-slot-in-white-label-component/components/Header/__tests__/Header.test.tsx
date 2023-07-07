3a9ea825e467bac9ba03b1dda62581f8458ef7dd
mister. jones <misterjones@users.noreply.github.com>
2023-07-07 14:32:44 -0400

    Rename Header test description

--- packages/vital-examples/src/no-slot-in-white-label-component/components/Header/__tests__/Header.test.tsx
+++ packages/vital-examples/src/no-slot-in-white-label-component/components/Header/__tests__/Header.test.tsx
@@ -3,4 +3,4 @@ import { testTokens } from '@bodiless/vital-elements';
 import { HeaderClean } from '@bodiless/vital-layout';
 import { exampleHeader } from '..';
 
-describe.skip('Copyright Row Tokens', testTokens(HeaderClean, exampleHeader));
+describe.skip('Header Tokens', testTokens(HeaderClean, exampleHeader));
