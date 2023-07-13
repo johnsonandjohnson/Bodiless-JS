0069d0299cc187712a849dcdc95557a02bc11315
Dewen Li <dewen.sinocrest@gmail.com>
2023-07-13 14:46:23 -0400

    package check

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 echo Postinstall on $(date) $(pwd)
 
+echo 'before build'
+find packages -type d
+
 echo patch-package
 npm run patch-package
 
 echo Building package...
 npm run build:packages
+
+echo 'after build'
+find packages -type d
