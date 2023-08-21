58fd2e81c760785eed30131d2be7e7950d2a839a
Samuel Davis <hello@smdvs.com>
2023-08-21 16:22:52 -0500

    Fix linting error

--- packages/vital-card/src/components/HeroCard/__tests__/HeroCard.test.tsx
+++ packages/vital-card/src/components/HeroCard/__tests__/HeroCard.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { testTokens } from '@bodiless/vital-elements';
-import { HeroCardClean } from '@bodiless/vital-card';
-import { vitalHeroCard } from '..';
+import { vitalHeroCard, HeroCardClean } from '..';
 
 // eslint-disable-next-line jest/valid-describe
 describe('HeroCard Tokens', testTokens(HeroCardClean, vitalHeroCard));
