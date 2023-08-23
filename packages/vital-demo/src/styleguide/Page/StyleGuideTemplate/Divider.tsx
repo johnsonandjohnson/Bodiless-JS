79ecc4575371c7a68b34fccabd71d7cbb411ec63
Samuel Davis <hello@smdvs.com>
2023-08-23 09:11:52 -0500

    Fix linting error

--- packages/vital-demo/src/styleguide/Page/StyleGuideTemplate/Divider.tsx
+++ packages/vital-demo/src/styleguide/Page/StyleGuideTemplate/Divider.tsx
@@ -27,9 +27,9 @@ const DemoFlowContainer = asFluidToken({
   Components: {
     Primary: on(DividerClean)(vitalDividers.Base, vitalDividers.WithPrimaryDivider),
     Secondary: on(DividerClean)(vitalDividers.Base, vitalDividers.WithSecondaryDivider),
-// eslint-disable-next-line max-len
+    // eslint-disable-next-line max-len
     VerticalPrimary: on(DividerClean)(vitalDividers.Base, vitalDividers.WithPrimaryDivider, vitalDividers.WithVerticalOrientation),
-// eslint-disable-next-line max-len
+    // eslint-disable-next-line max-len
     VerticalSecondary: on(DividerClean)(vitalDividers.Base, vitalDividers.WithSecondaryDivider, vitalDividers.WithVerticalOrientation),
   },
 });
